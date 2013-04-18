1149a661f3d049e04d43b36f82086c1ee411dd61
pdroid <pdroid@MS-7596-ubuntu.(none)>
2013-04-18 23:35:37 +0200

    add prebuilt (files from wiko .img & MTK sources)

--- device_s8073.mk
+++ device_s8073.mk
@@ -13,6 +13,11 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_s8073
 PRODUCT_DEVICE := s8073
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.secure=0 \
+    persist.mtk.aee.aed=on \
+    persist.sys.usb.config=mass_storage,adb \
+    persist.service.acm.enable=0
 
 #Permissions device files
 PRODUCT_COPY_FILES += \
